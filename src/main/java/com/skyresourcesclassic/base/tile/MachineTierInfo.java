e65668a277f23cf711672da87803f9fcb1096e70
EmosewaPixel <awesomeggaming@gmail.com>
2018-12-22 18:29:29 +0100

    Git missed this one

--- src/main/java/com/skyresourcesclassic/base/tile/MachineTierInfo.java
+++ src/main/java/com/skyresourcesclassic/base/tile/MachineTierInfo.java
@@ -1,4 +1,15 @@
 package com.skyresourcesclassic.base.tile;
 
 public class MachineTierInfo {
+    public String name;
+    public float hardness;
+    public float resistance;
+    public int id;
+
+    public MachineTierInfo(String name, float hardness, float resistance, int id) {
+        this.name = name;
+        this.hardness = hardness;
+        this.resistance = resistance;
+        this.id = id;
+    }
 }
