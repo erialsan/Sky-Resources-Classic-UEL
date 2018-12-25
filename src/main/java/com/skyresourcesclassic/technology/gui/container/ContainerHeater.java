06bc4bb64f71316725a1e22c137b33885acdba8d
EmosewaPixel <awesomeggaming@gmail.com>
2018-12-26 00:57:10 +0100

    Git forgot another one

--- src/main/java/com/skyresourcesclassic/technology/gui/container/ContainerHeater.java
+++ src/main/java/com/skyresourcesclassic/technology/gui/container/ContainerHeater.java
@@ -2,11 +2,11 @@ package com.skyresourcesclassic.technology.gui.container;
 
 import com.skyresourcesclassic.base.gui.ContainerBase;
 import com.skyresourcesclassic.base.gui.SlotSpecial;
-import com.skyresourcesclassic.technology.tile.TileCombustionHeater;
+import com.skyresourcesclassic.technology.tile.TileHeater;
 import net.minecraft.inventory.IInventory;
 
-public class ContainerCombustionHeater extends ContainerBase {
-    public ContainerCombustionHeater(IInventory playerInv, TileCombustionHeater te) {
+public class ContainerHeater extends ContainerBase {
+    public ContainerHeater(IInventory playerInv, TileHeater te) {
         super(playerInv, te);
         this.addSlotToContainer(new SlotSpecial(tile.getInventory(), 0, 80, 53));
     }
