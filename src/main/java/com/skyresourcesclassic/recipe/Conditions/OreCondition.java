77acc4b79bdf9b2424703d8b74b4af84c5e64e31
EmosewaPixel <awesomeggaming@gmail.com>
2018-12-26 22:36:54 +0100

    Add modular recipes for steel machines

---
+++ src/main/java/com/skyresourcesclassic/recipe/Conditions/OreCondition.java
@@ -0,0 +1,18 @@
+package com.skyresourcesclassic.recipe.Conditions;
+
+import com.google.gson.JsonObject;
+import net.minecraft.util.JsonUtils;
+import net.minecraftforge.common.crafting.IConditionFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+import net.minecraftforge.oredict.OreDictionary;
+
+import java.util.function.BooleanSupplier;
+
+public class OreDictCondition implements IConditionFactory {
+
+    @Override
+    public BooleanSupplier parse(JsonContext context, JsonObject json) {
+        String ore = JsonUtils.getString(json, "ore");
+        return () -> !OreDictionary.getOres(ore).isEmpty();
+    }
+}
